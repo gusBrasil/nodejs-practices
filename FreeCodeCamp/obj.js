abeca02d03436b13246b4684729d5d8e035fb935
gusBrasil <gustavomedbra@gmail.com>
2022-04-28 16:08:41 -0300

    update

---
+++ FreeCodeCamp/obj.js
@@ -0,0 +1,13 @@
+const someObj = {
+    propName: "John"
+  };
+  
+  function propPrefix(str) {
+    const s = "prop";
+    return s + str;
+  }
+  
+  const someProp = propPrefix("Name");
+  console.log(someObj[someProp]);
+
+  
---
+++ calc.js
@@ -0,0 +1,40 @@
+const prompt = require('prompt-sync')();
+
+let x = prompt("Somar, subtrair, multiplicar, dividir ou elevar? (1/2/3/4/5): ");
+let a = parseFloat(prompt("Fale o primeiro número "));
+let b = parseFloat(prompt("Fale o segundo número "));
+
+function sum() {
+    console.log(a + b);
+}
+
+function subtract() {
+    console.log(a - b);
+}
+
+function multiply() {
+    console.log(a * b);
+}
+
+function divide() {
+    console.log(a / b);
+}
+
+function exponential() {
+    console.log(a ** b);
+}
+
+if (x == 1) {
+    sum();
+} else if (x == 2) {
+    subtract();
+} else if (x == 3) {
+    multiply();
+} else if (x == 4) {
+    divide();
+} else if (x == 5) {
+    exponential();
+}
+ else if (x !== 1 || x !== 2 || x !== 3 || x !== 4 || x !== 5) {
+    console.log("por favor, escolha uma das operações disponíveis");
+}
