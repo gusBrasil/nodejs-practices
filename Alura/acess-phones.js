81e8987ae46408f6e6dc51c9a31110ca8789cb19
gusBrasil <gustavomedbra@gmail.com>
2022-04-29 10:02:12 -0300

    update

---
+++ Alura/acess-phones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: 'Gustavo',
+    idade: 20,
+    cpf: '12345678',
+    email: 'gustavo@email.com',
+    fones: ['55912345678', '55987654321']
+}
+
+cliente.fones.forEach(fone => console.log(fone))
