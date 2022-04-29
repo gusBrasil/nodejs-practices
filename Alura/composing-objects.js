5563428f2e2049ee4af5ca8ac974d6c4d822b077
gusBrasil <gustavomedbra@gmail.com>
2022-04-29 10:05:41 -0300

    update

---
+++ Alura/composing-objects.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: 'Gustavo',
+    idade: 20,
+    cpf: '12345678',
+    email: 'gustavo@email.com',
+    fones: ['55912345678', '55987654321']
+}
+
+cliente.dependentes = {
+    nome: 'Sara',
+    parentesco: 'Filha',
+    dataNasc: '20/03/2011'
+}
+
+console.log(cliente);
