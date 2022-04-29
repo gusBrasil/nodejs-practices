4e0f676b8c693a3054e092bc6732b96d3fc9c183
gusBrasil <gustavomedbra@gmail.com>
2022-04-29 10:29:56 -0300

    update

---
+++ Alura/making-deposits.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: 'Gustavo',
+    idade: 20,
+    cpf: '12345678',
+    email: 'gustavo@email.com',
+    fones: ['55912345678', '55987654321'],
+    dependentes: [
+        {
+            nome: 'Sara Silva',
+            parentesco: 'Filha',
+            dataNasc: '20/03/2011' },
+        {
+            nome: 'Samia Maria',
+            parentesco: 'filha',
+            dataNasc: '04/01/2014' }
+    ],
+}
+
+
