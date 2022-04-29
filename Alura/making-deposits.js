a295ecfe6f5edce28ed066dd151d08781e855dab
gusBrasil <gustavomedbra@gmail.com>
2022-04-29 10:34:37 -0300

    update

--- Alura/making-deposits.js
+++ Alura/making-deposits.js
@@ -14,6 +14,15 @@ const cliente = {
             parentesco: 'filha',
             dataNasc: '04/01/2014' }
     ],
+    saldo: 100,
+
+    depositar: function(valor){
+        this.saldo += valor
+    }
 }
 
+console.log(cliente.saldo);
 
+cliente.depositar(100);
+
+console.log(cliente.saldo);
