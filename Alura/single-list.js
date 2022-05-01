2d815d6ecb9f15227e4ed7c08c449fb5dc0412be
gusBrasil <gustavomedbra@gmail.com>
2022-04-30 21:24:18 -0300

    update

---
+++ Alura/single-list.js
@@ -0,0 +1,35 @@
+const clientes = [
+{
+        nome: 'Gustavo',
+        idade: 20,
+        cpf: '12345678',
+        email: 'gustavo@email.com',
+        fones: ['55912345678', '55987654321'],
+        dependentes: [
+            {
+                nome: 'Sara Silva',
+                parentesco: 'Filha',
+                dataNasc: '20/03/2011' },
+            {
+                nome: 'Samia Maria',
+                parentesco: 'filha',
+                dataNasc: '04/01/2014' }
+        ],
+        saldo: 100,
+    
+        depositar: function(valor){
+            this.saldo += valor
+        }
+    },
+    {
+        nome: 'Anna Julia',
+        cpf: '87654321',
+        dependentes: [{
+            nome: 'Sophia',
+            parentesco: 'filha',
+            dataNasc: '30/08/2020'
+        }]
+    }
+]
+
+const listaDependentes = 
