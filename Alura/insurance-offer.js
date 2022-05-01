1e2eef70fe6b2662cc6ad92169aadd59dc9f7a63
gusBrasil <gustavomedbra@gmail.com>
2022-04-30 21:14:00 -0300

    update

---
+++ Alura/insurance-offer.js
@@ -0,0 +1,31 @@
+const cliente = {
+    nome: 'Gustavo',
+    idade: 20,
+    cpf: '12345678',
+    email: 'gustavo@email.com',
+    fones: ['55912345678', '55987654321'],
+    dependentes: [
+        {
+            nome: 'Sara Silva',
+            parentesco: 'Filha',
+            dataNasc: '20/03/2011' },
+        {
+            nome: 'Samia Maria',
+            parentesco: 'filha',
+            dataNasc: '04/01/2014' }
+    ],
+    saldo: 100,
+
+    depositar: function(valor){
+        this.saldo += valor
+    }
+}
+
+function offerSeguro(obj) {
+    const propsClientes = Object.keys(cliente);
+    if(propsClientes.includes('dependentes')){
+        console.log(`oferta de seguro de vida para ${obj.nome}`)
+    }
+}
+
+offerSeguro(cliente);
