630801ce9b7da49f4afb5d02f316ce53a35c9af2
gusBrasil <gustavomedbra@gmail.com>
2022-05-02 09:24:50 -0300

    update

--- Alura/generic-customer.js
+++ Alura/generic-customer.js
@@ -1,1 +1,11 @@
-    
+function cliente(nome, cpf, email, saldo) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.saldo = saldo;
+    this.depositar = function(valor){
+        this.saldo += valor;
+    }
+}
+
+console.log(cliente(gustavo, 121, gustavogustavocom, 12));
