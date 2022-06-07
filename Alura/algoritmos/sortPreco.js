95c99c03701bd1f2ec8a43c2eb47540426452217
gusBrasil <gustavomedbra@gmail.com>
2022-06-07 03:51:58 -0300

    Update

---
+++ Alura/algoritmos/sortPreco.js
@@ -0,0 +1,9 @@
+let maisBarato = 0;
+
+for (let atual = 0; atual < precosLivros.length; atual++) {
+    if (precosLivros[atual] < precosLivros[maisBarato]) {
+        maisBarato = atual;
+    }
+}
+
+console.log(`o livro mais barato custa ${precosLivros[maisBarato]}`);
