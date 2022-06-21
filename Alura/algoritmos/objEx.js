a2da6ec83da17372bbea9a92b56cdfaa8da15c91
gusBrasil <gustavomedbra@gmail.com>
2022-06-21 10:50:40 -0300

    update

---
+++ Alura/algoritmos/objEx.js
@@ -0,0 +1,9 @@
+const precosLivros = require('./listaLivros');
+
+let x = parseFloat('5');
+
+if (5 === precosLivros[6].preco) {
+    console.log('Igual!');
+} else {
+    console.log('Diferente!');
+}
